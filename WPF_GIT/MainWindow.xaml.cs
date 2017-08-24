43e8aededc48473152219c952aaec107aff5e5b5
yas15 <31019957+yas15@users.noreply.github.com>
2017-08-24 23:44:57 +0100

    Add project files.

---
+++ WPF_GIT/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+
+namespace WPF_GIT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
